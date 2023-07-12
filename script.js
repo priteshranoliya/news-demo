ee5fe58908a7994379cbae5f0ce59c8d06aa3553
Pritesh Ranoliya <91262576+priteshranoliya@users.noreply.github.com>
2023-07-12 12:53:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "4c80016203104ac3ae163ea9881ec71e";
+const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
